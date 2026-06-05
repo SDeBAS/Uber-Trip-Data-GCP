408ecd53fb2fd4ba7a3b8f2512ff013fcca21cd2
Debanjan Basu <62347601+SDeBAS@users.noreply.github.com>
2026-06-05 17:58:38 +0530

    Update analytics_query.sql

--- analytics_query.sql
+++ analytics_query.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `data-with-darshil.uber_data_engineering_yt.tbl_analytics` AS (
+CREATE OR REPLACE TABLE `tbl_analytics` AS (
 SELECT 
 f.trip_id,
 f.VendorID,
@@ -21,9 +21,9 @@ f.improvement_surcharge,
 f.total_amount
 FROM 
 
-`data-with-darshil.uber_data_engineering_yt.fact_table` f
-JOIN `data-with-darshil.uber_data_engineering_yt.datetime_dim` d  ON f.datetime_id=d.datetime_id
-JOIN `data-with-darshil.uber_data_engineering_yt.passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
+`fact_table` f
+JOIN `datetime_dim` d  ON f.datetime_id=d.datetime_id
+JOIN `passenger_count_dim` p  ON p.passenger_count_id=f.passenger_count_id  
 JOIN `data-with-darshil.uber_data_engineering_yt.trip_distance_dim` t  ON t.trip_distance_id=f.trip_distance_id  
 JOIN `data-with-darshil.uber_data_engineering_yt.rate_code_dim` r ON r.rate_code_id=f.rate_code_id  
 JOIN `data-with-darshil.uber_data_engineering_yt.pickup_location_dim` pick ON pick.pickup_location_id=f.pickup_location_id
